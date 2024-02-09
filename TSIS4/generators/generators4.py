f97a2803cdb528b36934840938a9a9060ee53d2a
Alina Abibova <alinaten2103@gmail.com>
2024-02-09 18:30:00 +0500

    Added TSIS4

---
+++ TSIS4/generators/generators4.py
@@ -0,0 +1,8 @@
+def squares_generator(a, b):
+    for i in range(a, b + 1):  
+        yield i ** 2  
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a second number: "))
+for num in squares_generator(a, b):
+    print(num, end=" ")
